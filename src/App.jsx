da2e2244e619be797cbef150d343d07ddf63c284
Hanna Martysiuk <annamartysiuk7@gmail.com>
2026-01-06 20:58:24 +0100

    navigation-fetch-behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import TodosCardManual from "./components/TodosCardManual";
+import { Routes, Route, NavLink, Navigate } from "react-router-dom";
+import TodosPage from "./pages/TodosPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 
 export default function App() {
   return (
-    <div>
-      <h2>Experiment 1a — manual (10 components)</h2>
+    <div className="page">
+      <h2>Experiment 1b — manual</h2>
+      {/* NavLink выполняет навигацию и изменяет URL, тогда как маршрутизатор интерпретирует текущий путь и определяет, какой компонент должен быть отрисован. */}
+      <nav className="nav">
+        <NavLink to="/">/todos</NavLink>
+        <NavLink to="/about">/about</NavLink>
+      </nav>
 
-      <div className="cards">
-        {Array.from({ length: 10 }).map((_, i) => (
-          <TodosCardManual key={i} index={i + 1} />
-        ))}
-      </div>
+      {/* Компонент (TodosPage) — это описание / функция компонента, а не отрисованный элемент.
+
+JSX-элемент (<TodosPage />) — это конкретный экземпляр компонента, который React может смонтировать.при совпадении маршрута должен быть создан и отрисован конкретный экземпляр компонента */}
+      <Routes>
+        <Route path="/" element={<TodosPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/TodosPage.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+export default function TodosPage() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Lista zadań (manual)</h3>
+
+      <ul>
+        {todos.slice(0, 10).map((todo) => (
+          <li key={todo.id}>
+            #{todo.id} {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
